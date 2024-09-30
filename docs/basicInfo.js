6b4124618cfb5ec408a96f43d8845fa47c9d4e26
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-09-30 19:51:52 +0200

    modificacion en la tipografia del archivo basicInfo.js

--- docs/basicInfo.js
+++ docs/basicInfo.js
@@ -2,7 +2,7 @@ module.exports = {
     openapi:"3.0.3",
     info:{
         version:"1.0.0",
-        title:"Users",
-        description: "Users API"
+        title:"Task",
+        description: "Task API"
     }
 }
